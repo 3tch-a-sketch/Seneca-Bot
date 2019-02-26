bfa6b57e3b38c6bfcc652b0357097d997c020b4d
3tch-a-sketch <sametchells@icloud.com>
2019-02-26 17:17:18 +0000

    Added Movement

--- python/mouse_test.py
+++ python/mouse_test.py
@@ -3,3 +3,11 @@ import time
 
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
+
+width, height = pyautogui.size()
+
+for i in range(3):
+      pyautogui.moveTo(100, 100, duration=0.25)
+      pyautogui.moveTo(200, 100, duration=0.25)
+      pyautogui.moveTo(200, 200, duration=0.25)
+      pyautogui.moveTo(100, 200, duration=0.25)
