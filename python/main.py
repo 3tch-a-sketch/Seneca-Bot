17db6e9373783a331d8d6e3bd4a3519fad64f1bd
3tch-a-sketch <sametchells@icloud.com>
2019-02-26 19:30:57 +0000

    open ie fullscreen

--- python/main.py
+++ python/main.py
@@ -4,7 +4,5 @@ import minilib as mouse
 width, height = win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1)
 print(width, height)
 
-mouse.moveClick(0, width -10, 0.1)
-time.sleep(0.5)
-keyboard.write('Internet Explorer')
-keyboard.press('enter')
+keyboard.press_and_release("left windows+r")
+keyboard.write(iexplore -k)
