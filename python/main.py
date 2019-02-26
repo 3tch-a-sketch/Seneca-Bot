2a01f6d3f1d832a92ac88a5fda93a2194218db89
3tch-a-sketch <sametchells@icloud.com>
2019-02-26 20:13:30 +0000

    added timing

--- python/main.py
+++ python/main.py
@@ -5,4 +5,6 @@ width, height = win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1)
 print(width, height)
 
 keyboard.press_and_release("left windows+r")
+time.sleep(0.2)
 keyboard.write("iexplore https://app.senecalearning.com/login -k")
+keyboard.press_and_release("enter")
