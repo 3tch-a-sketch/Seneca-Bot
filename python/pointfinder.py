748c920a5adc20848b4e46d705c8142f8010ede9
3tch-a-sketch <sametchells@icloud.com>
2019-02-26 19:50:19 +0000

    done!

--- python/pointfinder.py
+++ python/pointfinder.py
@@ -3,3 +3,9 @@ import minilib as mouse
 
 width, height = win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1)
 print(width, height)
+
+while True:
+    Xpos, Ypos = mouse.pos()
+    Xrel = width / Xpos
+    Yrel = height / Ypos
+    print(Xrel + ' ' + Yrel)
