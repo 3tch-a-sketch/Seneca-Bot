80b353228a53a507738ea4e3b0cfa2ddee97ad9c
3tch-a-sketch <sametchells@icloud.com>
2019-02-26 17:29:15 +0000

    Added a more commands

--- python/mouse_test.py
+++ python/mouse_test.py
@@ -7,9 +7,17 @@ pyautogui.FAILSAFE = True
 width, height = pyautogui.size()
 
 mouseX, moouseY = pyautogui.position()
+
 for i in range(3):
       pyautogui.moveTo(100, 100, duration=0.1)
       pyautogui.moveTo(200, 100, duration=0.1)
       pyautogui.moveTo(200, 200, duration=0.1)
       pyautogui.moveTo(100, 200, duration=0.1)
       pyautogui.moveRel(0, -100, duration=0.25)
+
+pyautogui.click(200, 250, button='right')
+
+pyautogui.mouseDown()
+pyautogui.mouseup()
+
+pyautogui.doubleClick()
