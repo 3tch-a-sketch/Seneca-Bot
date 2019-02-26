7170e4d9870429f087c100e2812f77d094ac6ac8
MatthewIsAwesome <matthewmiles2@gmail.com>
2019-02-26 21:09:55 +0000

    sfdsf

--- javascript/answer.js
+++ javascript/answer.js
@@ -7,5 +7,11 @@ function getClass(name) {
 }
 
 blocks = getClass("ModuleStyleWrapper__wrapper");
-currentBlock = blocks[blocks.length - 1].contents;
-document.write(currentBlock);
+currentBlock = blocks[blocks.length - 1];
+type = currentBlock.children[0].className;
+
+if(type == "ModuleFeedbackWrapper") { // Hidden word from list
+  blurredItems = currentBlock.getElementsByClassName("BlurredWord__word BlurredWord__word--blurred")
+  for(i = 0; i < blurredItems)
+  blurredItem = [0].children[0].innerHTML;
+}
