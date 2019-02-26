e1696eb7116ab63d74271cf7b31ff8732da2f9de
3tch-a-sketch <sametchells@icloud.com>
2019-02-26 17:23:26 +0000

    mouse pos

--- python/mouse_test.py
+++ python/mouse_test.py
@@ -6,10 +6,10 @@ pyautogui.FAILSAFE = True
 
 width, height = pyautogui.size()
 
+mouseX, moouseY = pyautogui.position()
 for i in range(3):
       pyautogui.moveTo(100, 100, duration=0.1)
       pyautogui.moveTo(200, 100, duration=0.1)
       pyautogui.moveTo(200, 200, duration=0.1)
       pyautogui.moveTo(100, 200, duration=0.1)
       pyautogui.moveRel(0, -100, duration=0.25)
-      
