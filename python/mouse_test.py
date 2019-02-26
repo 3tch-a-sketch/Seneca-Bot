de462a7968345f4f660e6c93d5afa166e138108b
3tch-a-sketch <sametchells@icloud.com>
2019-02-26 17:21:55 +0000

    added relmove

--- python/mouse_test.py
+++ python/mouse_test.py
@@ -7,7 +7,9 @@ pyautogui.FAILSAFE = True
 width, height = pyautogui.size()
 
 for i in range(3):
-      pyautogui.moveTo(100, 100, duration=0.25)
-      pyautogui.moveTo(200, 100, duration=0.25)
-      pyautogui.moveTo(200, 200, duration=0.25)
-      pyautogui.moveTo(100, 200, duration=0.25)
+      pyautogui.moveTo(100, 100, duration=0.1)
+      pyautogui.moveTo(200, 100, duration=0.1)
+      pyautogui.moveTo(200, 200, duration=0.1)
+      pyautogui.moveTo(100, 200, duration=0.1)
+      pyautogui.moveRel(0, -100, duration=0.25)
+      
