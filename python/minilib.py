577735f65f9aeb7b7bb5e5a5fc1cca4872c48763
3tch-a-sketch <sametchells@icloud.com>
2019-02-26 18:22:37 +0000

    Added timings

--- python/minilib.py
+++ python/minilib.py
@@ -1,6 +1,11 @@
-import win32api, win32con
+import win32api, win32con, time
 
-def click(x,y):
+def click(x,y,t):
     win32api.SetCursorPos((x,y))
+    time.sleep(t)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    time.sleep(t)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+
+def move(x,y,t):
+    
