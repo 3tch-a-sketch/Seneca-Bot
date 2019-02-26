f72a3c2404deea08916f10556af48c39ffa4ec96
MatthewIsAwesome <matthewmiles2@gmail.com>
2019-02-26 18:36:08 +0000

    Added more parts

--- python/minilib.py
+++ python/minilib.py
@@ -1,12 +1,26 @@
 import win32api, win32con, time
 
-def click(x,y,t):
-    win32api.SetCursorPos((x,y))
+def moveClick(x, y, t=0):
+    win32api.SetCursorPos((x, y))
     time.sleep(t)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
     time.sleep(t)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
-def move(x,y,t):
-    win32api.SetCursorPos((x,y))
+def hover(x, y, t=0):
+    win32api.SetCursorPos((x, y))
     time.sleep(t)
+
+def click(x, y, t=0):
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+    time.sleep(t)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+
+def drag(x1, y1, x2, y2, t=0):
+    win32api.SetCursorPos((x1, y1))
+    time.sleep(t)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x1, y1, 0, 0)
+    time.sleep(t)
+    win32api.SetCursorPos((x2, y2))
+    time.sleep(t)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x2, y2, 0, 0)
