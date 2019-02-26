47111f29af0d77a8155b611498736b1188096830
3tch-a-sketch <sametchells@icloud.com>
2019-02-26 20:27:16 +0000

    addded login

--- python/main.py
+++ python/main.py
@@ -1,12 +1,22 @@
 import time, keyboard, win32api, win32con, win32gui
 import minilib as mouse
 
+email = input('email:')
+password = input("password:")
+globaldelay = 0.2   #this should be higher on slower computers.
+
 width, height = win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1)
-print(width, height)
+#print(width, height)
 
 keyboard.press_and_release("left windows+r")
-time.sleep(0.2)
+time.sleep(globaldelay)
 keyboard.write("iexplore -k https://app.senecalearning.com/login")
 keyboard.press_and_release("enter")
-time.sleep(0.2)
-keyboard.press_and_release("f11")
+time.sleep(globaldelay)
+keyboard.write(email)
+del email
+time.sleep(globaldelay)
+keyboard.press_and_release("tab")
+time.sleep(globaldelay)
+keyboard.write(password)
+del password
